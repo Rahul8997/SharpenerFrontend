a769f56f738a10d4c9d15c1d63f49b89db14c170
Rahul8997 <rdprajapati8997@gmail.com>
2023-01-21 01:56:16 +0530

    Advance React Task-8 Added

---
+++ FrontEnd-Advance React-Task-8/src/App.js
@@ -0,0 +1,49 @@
+import "./App.css";
+import NewExpense from "./Components/Expenses/NewExpense";
+import ExpenseItems from "./Components/Expenses/ExpenseItems";
+const App=()=> {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  const addExpensehandler=(expense)=>{
+    console.log("In app.js");
+    console.log(expense);
+
+    expenses.push(
+    { id: expense.id,
+    title: expense.title,
+    amount: expense.amount,
+    date: expense.date});
+  }
+
+  return (
+    <div style={{backgroundColor:"gray"}}>
+      <h1>Lets get started!!</h1>
+     <NewExpense onAddExpense={addExpensehandler}/>
+      {expenses.map((ele)=>{
+        return <ExpenseItems key={ele.id} exptitle={ele.title} expamount={ele.amount} expdate={ele.date}/>
+      })}
+    </div>
+  );
+}
+
+export default App;
