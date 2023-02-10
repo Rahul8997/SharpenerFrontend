a65ea5ad7227422d3470ddc0ab360f1c0f28e14f
Rahul8997 <rdprajapati8997@gmail.com>
2023-02-10 22:23:05 +0530

    FrontEnd-Creating restaurent website-Task-1 Added

---
+++ FrontEnd-Creating restaurent website Task-1/src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+import Header from './Components/Layout/Header';
+
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
+}
+
+export default App;
