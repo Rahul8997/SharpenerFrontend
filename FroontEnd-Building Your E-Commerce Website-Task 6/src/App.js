cad1577239350cdfe7a9576e56c8459c0ec82bc5
Rahul8997 <rdprajapati8997@gmail.com>
2023-02-19 03:19:49 +0530

    FrontEnd-Building Your E-COmmerce Website-Task-6 Added

---
+++ FroontEnd-Building Your E-Commerce Website-Task 6/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+import MoviesList from './Components/MovieList';
+import './App.css';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+
+  async function fetchMoviesHandler() {
+    let responce = await fetch('https://swapi.dev/api/films/');
+    responce = await responce.json();
+    const transformedMovies = responce.results.map((movieData) => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date,
+      }
+    })
+    setMovies(transformedMovies);
+  };
+
+  return (
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
+  );
+}
+
+export default App;
