1b8d432c9d937a1ee3c6cf9baeba3bacbd89841b
Rahul8997 <rdprajapati8997@gmail.com>
2023-02-20 23:32:20 +0530

    FrontEnd-Building Your E-COmmerce Website-Task-14 Added

---
+++ FroontEnd-Building Your E-Commerce Website-Task 14/src/pages/AllQuotes.js
@@ -0,0 +1,13 @@
+
+import QuoteList from '../Components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+  { id: 'q1', author: 'Max', text: 'Learning React is fun!' },
+  { id: 'q2', author: 'Maximilian', text: 'Learning React is great!' },
+];
+
+const AllQuotes = () => {
+  return <QuoteList quotes={DUMMY_QUOTES} />
+};
+
+export default AllQuotes;
