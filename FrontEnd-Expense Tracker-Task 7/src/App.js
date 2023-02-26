15f0ca3a3deae9772c3a82591cac1c6216cfef2f
Rahul8997 <rdprajapati8997@gmail.com>
2023-02-26 14:39:38 +0530

    FrontEnd-Expense Tracker-Task 7 Added

---
+++ FrontEnd-Expense Tracker-Task 7/src/App.js
@@ -0,0 +1,32 @@
+import { Fragment } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import DummyScreen from './Components/Layout/DummyScreen';
+import Navbar from './Components/Layout/Navbar'
+import ProfileDetails from './Components/Layout/ProfileDetails';
+import ForgetPasswordPage from './Components/Pages/ForgetPasswordPage';
+import Login from './Components/Pages/Login';
+function App() {
+  return (
+    <Fragment>
+      <Navbar />
+      <Switch>
+        <Route exact path='/'>
+          <Login />
+        </Route>
+        <Route exact path='/forget'>
+          <ForgetPasswordPage />
+        </Route>
+        <Route exact path='/profile'>
+          <DummyScreen />
+        </Route>
+        <Route exact path='/details'>
+          <ProfileDetails />
+        </Route>
+
+      </Switch>
+
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ FrontEnd-Expense Tracker-Task 7/src/Components/Layout/Navbar.js
@@ -0,0 +1,49 @@
+import React, { useContext } from 'react'
+import { useHistory } from 'react-router-dom';
+import { expContext } from '../Store/ExpenseContext';
+const Navbar = () => {
+    const history = useHistory();
+    const ctx = useContext(expContext)
+    const handleLogout = (e) => {
+        e.preventDefault();
+        history.push('/')
+        ctx.setLogin(false);
+        ctx.setToken(null);
+        ctx.profileInfo({ myName: "", myUrl: "" });
+        console.log(ctx);
+    }
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active text-primary" aria-current="page" href="/">
+                                    MyWebLink
+                                    </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">Products</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">About Us</a>
+                            </li>
+                        </ul>
+                        {ctx.token && <form className="form-inline my-2 my-lg-0">
+                            <button className='btn btn-primary' onClick={handleLogout}>Logout</button>
+                        </form>}
+                    </div>
+                </div>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
