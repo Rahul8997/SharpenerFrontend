49521c47b7d7450f875e34f85fdbb22b8a3728ab
Rahul8997 <rdprajapati8997@gmail.com>
2023-03-02 21:27:52 +0530

    FrontEnd-Expense Tracker-Task 17 Added

---
+++ FrontEnd-Expense Tracker-Task 17/src/App.js
@@ -0,0 +1,42 @@
+import { Fragment } from 'react';
+import { useSelector } from 'react-redux';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import DummyScreen from './Components/Layout/DummyScreen';
+import Navbar from './Components/Layout/Navbar'
+import ProfileDetails from './Components/Layout/ProfileDetails';
+import Expenses from './Components/Pages/Expenses';
+import ForgetPasswordPage from './Components/Pages/ForgetPasswordPage';
+import Login from './Components/Pages/Login';
+function App() {
+  const token = useSelector(state => state.authentication.token);
+  const isLoggedIn = useSelector(state => state.authentication.isLoggedIn);
+  const mode= useSelector(state => state.theme.mode);
+  return (
+    <div  className={!mode?'bg-secondary text-light':''} style={{height:"100vh"}}>
+      <Navbar />
+      <div>
+      <Switch>
+        <Route exact path='/'>
+          <Login />
+        </Route>
+        <Route exact path='/forget'>
+          <ForgetPasswordPage />
+        </Route>
+        <Route exact path='/profile'>
+          {token && <DummyScreen />}
+          {!token && <Redirect to='/' />}
+        </Route>
+        <Route exact path='/details'>
+          <ProfileDetails />
+        </Route>
+        <Route exact path='/expenses'>
+          {isLoggedIn && <Expenses />}
+          {!isLoggedIn && <Redirect to='/' />}
+        </Route>
+      </Switch>
+      </div>
+    </div>
+  );
+}
+
+export default App;
